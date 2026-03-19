d9fa65bcda89b7ad4bd3d8433b9f15f1a9bd38b4
LarissaGoyal <larissagoyal@gmail.com>
2026-03-19 22:08:10 +0530

    added variables for various dimensions

--- tictactoe.py
+++ tictactoe.py
@@ -7,8 +7,8 @@ import classGame as cg
 
 pygame.init()
 
-width = 600
-height = 600
+width = 1280 
+height = 720
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Learning")
 clock = pygame.time.Clock()
@@ -46,28 +46,42 @@ game_over = False
 
 def win(name_of_winner):
     global bg_color
-    bg_color="green"
+    bg_color=(0,150,70)
     global game_over
     game_over=True
     print(name_of_winner, " wins")
 
+cell_size=60
+x_start = width//2 - 5*cell_size
+x_end = width//2+ 5*cell_size
+y_start = 5
+y_end = 5+(10*cell_size)
+filled=0
+r = (cell_size*7)//20
 while running:
     screen.fill(bg_color)
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
         if not game_over:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x,y = pygame.mouse.get_pos()
-                row = ((x-5)*10)//500
-                col = ((y-5)*10)//500
-                centre_x = 30+row*50
-                centre_y = 30+col*50
+                row = ((x-x_start)*10)//(10*cell_size)
+                col = ((y-y_start)*10)//(10*cell_size)
+                centre_x = x_start+(cell_size//2)+(row*cell_size)
+                centre_y = y_start+(cell_size//2)+(col*cell_size)
                 if (game.turn) and (game.board[row][col]==0):
                     circ.append((centre_x,centre_y))
                     game.switchTurn()
                     game.board[row][col]=1
+                    filled+=1
                     if game.checkWin(1):
                         win("player1")
 
@@ -75,31 +89,38 @@ while running:
                     cross.append((centre_x,centre_y))
                     game.switchTurn()
                     game.board[row][col]=-1
+                    filled+=1
                     if game.checkWin(-1):
                         win("player2")
-            
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-
+    
+    # Hover effect:
     x,y = pygame.mouse.get_pos()
-    row = ((x-5)*10)//500
-    col = ((y-5)*10)//500
+    row = ((x-x_start)*10)//(10*cell_size)
+    col = ((y-y_start)*10)//(10*cell_size)
     if row<10 and col<10 and row>=0 and col>=0:
-        pygame.draw.rect(screen, "blue", (5+row*50,5+col*50,50,50))
+        pygame.draw.rect(screen, "blue", (x_start+row*cell_size,y_start+col*cell_size,cell_size,cell_size))
+   
+    # 10x10 Grid:
+    for i in range(x_start, x_end+cell_size, cell_size):
+        pygame.draw.line(screen, (200,40,40), (i,y_start), (i,y_end), 5)
 
-    for i in range(5, 555, 50):
-        pygame.draw.line(screen, "red", (i,5), (i,505), 5)
+    for i in range(y_start, y_end+cell_size, cell_size):
+        pygame.draw.line(screen, "red", (x_start,i), (x_end,i), 5)
 
-    for i in range(5, 555, 50):
-        pygame.draw.line(screen, "red", (5,i), (505,i), 5)
-
+    # adding crosses
     for a, b in cross:
-        pygame.draw.line(screen, "white", (a - 18, b - 18), (a + 18, b + 18), 6)
-        pygame.draw.line(screen, "white", (a + 18, b - 18), (a - 18, b + 18), 6)
+        pygame.draw.line(screen, "white", (a - r, b - r), (a + r, b + r), 6)
+        pygame.draw.line(screen, "white", (a + r, b - r), (a - r, b + r), 6)
+
+    # adding circles
     for a, b in circ:
-        pygame.draw.circle(screen, "white", (a,b), 20, 6)
+        pygame.draw.circle(screen, "white", (a,b), r, 6)
+
+    # dealing with a tie
+    if (filled == 100) and (not game_over):
+        game_over=True
+        #print("it's a tie")
+
 
     pygame.display.flip()
     clock.tick(60)

520f6b8c9f085dfa366b4734edd162b100d07d0c
Sahil Deo <sahildeo54@gmail.com>
2026-03-24 15:19:07 +0530

    Removed class Game
    putting it in a different file

--- game.py
+++ game.py
@@ -10,19 +10,6 @@ username2=sys.argv[2]
 
 print("Welcome to the game, " + username1 + " and " + username2 + "!")
 
-class Game:
-    def __init__(self):
-		self.boardWIDTH=0 #x coordinate length
-		self.boardHEIGHT=0 #y coordinate length
-		self.board=[]
-		self.turn=0
-    def switchTurn(self):
-    	self.turn=not self.turn
-    def check_win(self):
-    	raise NotImplementedError
-	
-
-
 gameName=None
 
 import tictactoe as ttt
